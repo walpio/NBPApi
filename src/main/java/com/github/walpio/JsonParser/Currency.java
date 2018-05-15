ddf154406d702b9d233649c1fd6cfb3d4d7bb20d
walpio <doksmal@gmail.com>
2018-05-15 12:22:57 +0200

    rate POJO added

--- src/main/java/com/github/walpio/JsonParser/Currency.java
+++ src/main/java/com/github/walpio/JsonParser/Currency.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import java.util.List;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "table",
