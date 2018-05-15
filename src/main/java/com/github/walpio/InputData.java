d1d3da4e136cec4ca7f9eb15e5c96f720d5e6a5e
walpio <doksmal@gmail.com>
2018-05-15 11:38:24 +0200

    class to store user input created

---
+++ src/main/java/com/github/walpio/InputData.java
@@ -0,0 +1,32 @@
+package com.github.walpio;
+
+public class InputData {
+
+    private String currency;
+    private String startDate;
+    private String endDate;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
